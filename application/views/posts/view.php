d4a299094180f8f4db8d00d7f05534f8e696c3f4
joelalive <joelimalive1994@gmail.com>
2020-03-16 16:45:35 +0530

    PART 5: Comment Adding

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -12,3 +12,26 @@
 </form>
 
 <a class="btn btn-info pull-left" href="<?php echo base_url();?>posts/edit/<?= $post['slug'];?>">Edit</a>
+
+<hr>
+<h3>Add Comment</h3>
+
+<?php echo validation_errors();?>
+
+<?php echo form_open('comments/create/'.$post['id']); ?>
+
+<div class="form-group">
+<label>Name</label>
+<input type="text" name="name" class="form-control">
+</div>
+<div class="form-group">
+<label>Email</label>
+<input type="text" name="email" class="form-control">
+</div>
+<div class="form-group">
+<label>Body</label>
+<textarea id="editor1" rows="5" name="body" class="form-control" placeholder="Enter Content"></textarea>
+</div>
+<input type="hidden" name="slug" value="<?php echo $post['slug']; ?>" class="form-control">
+<button type="submit" class="btn btn-md btn-info">Submit</button>
+</form>
